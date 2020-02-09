428ce1551caba8e39981a2009f52f013c8d92960
Lewis Jones <lewisjones21@hotmail.com>
2020-02-09 18:51:41 +0000

    Split clock function into separate script

--- js/dashboard.js
+++ js/dashboard.js
@@ -31,10 +31,3 @@ async function updateEvent() {
 }
 
 updateEvent();
-
-const $time = document.querySelector(".time");
-function updateTime() {
-    $time.textContent = new Date().toLocaleTimeString().split(":").slice(0, -1).join(":");
-}
-setInterval(updateTime, 5000);
-updateTime();
